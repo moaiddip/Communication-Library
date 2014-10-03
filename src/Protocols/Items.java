1940fd6317bfb07031f4d869b69042e325f0668b
Denisa Biala <Denisa Biala@194.47.46.106>
2014-10-03 18:14:22 +0200

    Corrected a few mistakes.

--- src/Protocols/Items.java
+++ src/Protocols/Items.java
@@ -16,46 +16,73 @@ package Protocols;
 //reply: the answer to the query
 //synchronized methods are read/written by different threads
 public class Items {
-    boolean newOrOld=true;
-    boolean answered=false;//testing: true, default: false
+
+    boolean newOrOld = true;
+    boolean answered = false;//testing: true, default: false
     String message;
     String reply;
     String address;
+
     //sets the query of a client and the clients ip address in the items instance
-    public void create(String message, String address){
-        this.message=message;
-        this.address=address;
+
+    public void create(String message, String address) {
+        this.message = message;
+        this.address = address;
     }
+
     //checks if the query is answered, synchronized because it can be accessed
     //and changed by multiple threads
-    public synchronized Boolean isAnswered(){
-        return answered;
+
+    public Boolean isAnswered() {
+        synchronized (this) {
+            return answered;
+        }
     }
+
     //Gets the query
-    public String getMsg(){
+
+    public String getMsg() {
         return message;
     }
+
     //Gets the answer to the query
-    public synchronized String getReply(){
-        return reply;
+
+    public String getReply() {
+        synchronized (this) {
+            return reply;
+        }
     }
+
     //Makes a new query old so that it can be replaced in the hashmap
-    public synchronized void makeOld(){
-        newOrOld=!newOrOld;
+
+    public void makeOld() {
+        synchronized (this) {
+            newOrOld = !newOrOld;
+        }
     }
+
     //sets the reply message and sets the answered boolean to true (answered)
-    public synchronized void setReply(String message){
-        reply=message;
-        answered=true;
+
+    public void setReply(String message) {
+        synchronized (this) {
+            reply = message;
+            answered = true;
+        }
     }
+
     //returns if the message is new or old
     //a message is only set to old once the reply has been sent back
     //to the client ONLY
-    public synchronized Boolean getState(){
-        return newOrOld;
+
+    public Boolean getState() {
+        synchronized (this) {
+            return newOrOld;
+        }
     }
+
     //returns the ip address of the client who issued the query
-    public String getAddress(){
+
+    public String getAddress() {
         return address;
     }
 }
