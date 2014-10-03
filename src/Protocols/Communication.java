47f36d41d2eb0c6c3d0c679b41f972546b3c8a12
Denisa Biala <Denisa Biala@194.47.46.106>
2014-10-03 14:02:05 +0200

    Removed unused import.

--- src/Protocols/Communication.java
+++ src/Protocols/Communication.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.SocketAddress;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.SSLSocket;
