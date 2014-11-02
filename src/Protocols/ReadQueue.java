a06c077def7ec840f87f62b5a2218e72abe7e8eb
Sozos <Sozos@Sozos-pc>
2014-11-02 23:19:13 +0100

    Outdated.

--- src/Protocols/ReadQueue.java
+++ src/Protocols/ReadQueue.java
@@ -10,7 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Accessed by the server part that connects to the database to process queries.OUTDATED!
+ * OUTDATED!OUTDATED!OUTDATED!Accessed by the server part that connects to the database to process queries.
  * An example of the second half of the realtime queue implementation
  * Needs to be implemented by the Servers group.
  * 
