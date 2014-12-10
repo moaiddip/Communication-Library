ce000272e3a67a82cda085eb6ffb65f92d2c5c0e
Denisa Biala <Denisa Biala@194.47.46.124>
2014-12-10 23:36:40 +0100

    Double woops

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -290,7 +290,7 @@ public class Client extends Thread {
         Calendar cal = Calendar.getInstance();
         long time = cal.getTimeInMillis() / 1000;
         while (query.get() || working.get()) {
-            if (cal.getTimeInMillis() - time >= timeOut) {
+            if (cal.getTimeInMillis() /1000 - time >= timeOut) {
                 return;
             }
         }
@@ -336,7 +336,7 @@ public class Client extends Thread {
         Calendar cal = Calendar.getInstance();
         long time = cal.getTimeInMillis() / 1000;
         while (!finished.get()){
-            if (cal.getTimeInMillis() - time >= timeOut) {
+            if (cal.getTimeInMillis() /1000 - time >= timeOut) {
                 return null;
             }
         }
